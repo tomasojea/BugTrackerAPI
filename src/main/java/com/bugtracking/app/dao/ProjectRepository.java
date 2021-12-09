5859c6cecdbae3184c8d25932857cd30eb570a45
tomasojea <tomasojea0@gmail.com>
2021-12-09 18:10:54 +0000

    added cross origin on Project dao interface

--- src/main/java/com/bugtracking/app/dao/ProjectRepository.java
+++ src/main/java/com/bugtracking/app/dao/ProjectRepository.java
@@ -3,8 +3,10 @@ package com.bugtracking.app.dao;
 import com.bugtracking.app.entity.Project;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "projects",path = "projects")
+@CrossOrigin()
 public interface ProjectRepository extends CrudRepository<Project, Integer>{
     
 }
